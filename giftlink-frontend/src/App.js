a591a008288bf689a5fd9d6df660adf146d483b7
Jacob Warneke <jwarneke94@gmail.com>
2025-07-03 13:31:22 -0400

    Implement the Search Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
+import Navbar from './components/Navbar/Navbar';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
-  return (
+    const navigate = useNavigate();
+    return (
         <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-        </Routes>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/search" element={<SearchPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+            </Routes>
         </>
-  );
+    );
 }
 export default App;
